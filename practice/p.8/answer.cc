4de7d44c9b8990094083d6ad409273996449b824
Kiwamu Okabe <kiwamu@debian.or.jp>
2011-11-05 22:28:31 +0900

    add bin_search

--- practice/p.8/answer.cc
+++ practice/p.8/answer.cc
@@ -2,16 +2,35 @@
 #include <algorithm>
 using namespace std;
 
+int bin_search(int n, int k[], int f) {
+	int i = n / 2;
+
+	if (k[i] == f || k[n] == f) {
+		return YES;
+	}
+	if (n == 0 || i == 0) {
+		return NO;
+	}
+
+	if (k[i] < f) {
+		return bin_search(n - i, k + i, f);
+	} else {
+		return bin_search(i, k, f);
+	}
+}
+
 int solve(int n, int m, int k[]) {
-	int a, b, c, d;
+	int a, b, c;
+
+	sort(k, k + n);
 
 	for (a = 0; a < n; a++) {
 		for (b = 0; b < n; b++) {
 			for (c = 0; c < n; c++) {
-				for (d = 0; d < n; d++) {
-					if (m == k[a] + k[b] + k[c] + k[d]) {
-						return YES;
-					}
+				int f = m - k[a] - k[b] - k[c];
+				if (f > 0 &&
+				    YES == bin_search(n, k, f)) {
+					return YES;
 				}
 			}
 		}
