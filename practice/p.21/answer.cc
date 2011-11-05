47e6341f8a35d58f42e3c2fbc85154dbfbe64721
Kiwamu Okabe <kiwamu@debian.or.jp>
2011-11-05 17:19:06 +0900

    my answer for p.21

--- practice/p.21/answer.cc
+++ practice/p.21/answer.cc
@@ -1,5 +1,22 @@
 #include "answer.h"
+#include <algorithm>
+using namespace std;
 
 int solve(int n, int a[]) {
-	return 0;
+	int i, j, k, r = 0;
+
+	for (i = 0; i < n; i++) {
+		for (j = i + 1; j < n; j++) {
+			for (k = j + 1; k < n; k++) {
+				int s = a[i] + a[j] + a[k];
+				int m = max(a[i], max(a[j], a[k]));
+				int rest = s - m;
+				if (rest > m && s > r) {
+					r = s;
+				}
+			}
+		}
+	}
+
+	return r;
 }
